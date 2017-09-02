389b0c5a039a8be0bd3cd688de4fcbf0110fe33c
stognienkovladislav <stognienkovv@gmail.com>
2017-09-02 17:19:30 +0300

    Combining&Merging Data Sets

--- O`REILLY/DataWrangling/Combining_Merging_Data_Sets.py
+++ O`REILLY/DataWrangling/Combining_Merging_Data_Sets.py
@@ -98,3 +98,131 @@ righth = DataFrame(np.arange(12).reshape((6, 2)),
 
 print(lefth)
 print(righth)
+
+
+print(pd.merge(lefth, righth, left_on = ['key1', 'key2'], right_index = True))
+
+print(pd.merge(lefth, righth, left_on = ['key1', 'key2'],
+               right_index = True, how = 'outer'))
+
+
+
+left2 = DataFrame([[1., 2.], [3., 4.], [5., 6.]], index = ['a', 'c', 'e'],
+                  columns=['Ohio', 'Nevada'])
+
+right2 = DataFrame([[7., 8.], [9., 10.], [11., 12.], [13., 14.]],
+                   index = ['b', 'c', 'd', 'e'], columns=['Missouri', 'Alabama'])
+
+print(left2)
+print(right2)
+
+print(pd.merge(left2, right2, how = 'outer', left_index = True, right_index=True))
+
+
+print(left2.join(right2, how = 'outer'))
+
+print(left1.join(right1, on = 'key'))
+
+
+another = DataFrame([[7., 8.], [9., 10.], [11., 12.], [16., 17.]],
+                    index = ['a', 'c', 'e', 'f'], columns = ['New York', 'Oregon'])
+
+print(left2.join([right2, another]))
+
+print(left2.join([right2, another], how = 'outer'))
+
+
+#Concatenating Along an Axis
+
+arr = np.arange(12).reshape((3, 4))
+
+print(arr)
+
+print(np.concatenate([arr, arr], axis=1))
+
+print(np.concatenate((arr, arr), axis = 0))
+
+s1 = Series([0, 1], index = ['a', 'b'])
+
+s2 = Series([2, 3, 4], index = ['c', 'd', 'e'])
+
+s3 = Series([5, 6], index = ['f', 'g'])
+
+
+print(pd.concat([s1, s2, s3]))
+
+print(pd.concat([s1, s2, s3], axis = 1))
+
+s4 = pd.concat([s1 * 5, s3])
+
+print(pd.concat([s1, s4], axis = 1))
+
+print(pd.concat([s1, s4], axis = 1, join = 'inner'))
+
+
+
+print(pd.concat([s1, s4], axis = 1, join_axes=[['a', 'c', 'b', 'e']]))
+
+result = pd.concat([s1, s1, s3], keys = ['one', 'two', 'three'])
+
+print(result)
+
+
+#Much more on the unstack function later
+
+print(result.unstack())
+
+print(pd.concat([s1, s2, s3], axis = 1, keys = ['one', 'two', 'three']))
+
+df1 = DataFrame(np.arange(6).reshape(3, 2), index = ['a', 'b', 'c'],
+                columns=['one', 'two'])
+
+df2 = DataFrame(5 + np.arange(4).reshape(2, 2), index = ['a', 'c'],
+                columns=['three', 'four'])
+
+print(pd.concat([df1, df2], axis = 1, keys = ['level1', 'level2']))
+
+
+print(pd.concat([df1, df2], axis = 1, keys = ['level1', 'level2'],
+                names = ['upper', 'lower']))
+
+
+df1 = DataFrame(np.random.randn(3, 4), columns = ['a', 'b', 'c', 'd'])
+
+df2 = DataFrame(np.random.randn(2, 3), columns = ['b', 'd', 'a'])
+
+print(df1)
+print(df2)
+
+
+print(pd.concat([df1, df2], ignore_index=True))
+
+
+#Combining Data with Overlap
+
+
+a = Series([np.nan, 2.5, np.nan, 3.5, 4.5, np.nan],
+           index = ['f', 'e', 'd', 'c', 'b', 'a'])
+
+b = Series(np.arange(len(a), dtype=np.float64),
+           index = ['f', 'e', 'd', 'c', 'b', 'a'])
+
+b[-1] = np.nan
+
+print(a)
+print(b)
+print(np.where(pd.isnull(a), b, a))
+
+
+print(b[:-2].combine_first(a[2:]))
+
+
+df1 = DataFrame({'a' : [1., np.nan, 5., np.nan],
+                 'b' : [np.nan, 2., np.nan, 6.],
+                 'c' : range(2, 18, 4)})
+
+df2 = DataFrame({'a' : [5., 4., np.nan, 3., 7.],
+                 'b' : [np.nan, 3., 4., 6., 8.]})
+
+print(df1.combine_first(df2))
+
