f3d9ba180c27034aa3c9d12c1145cb3ccc24e6fa
stognienkovladislav <stognienkovv@gmail.com>
2017-12-30 18:38:42 +0200

    micro test

--- MatplotlibTasks/Wire3d_animation.py
+++ MatplotlibTasks/Wire3d_animation.py
@@ -1,6 +1,3 @@
-
-
-
 from __future__ import print_function
 
 from mpl_toolkits.mplot3d import axes3d
@@ -9,36 +6,36 @@ import numpy as np
 import time
 
 
-
 def generate(X, Y, phi):
+    '''
+    Generates Z data for the points in the X, Y meshgrid and parameter phi.
+    '''
     R = 1 - np.sqrt(X**2 + Y**2)
     return np.cos(2 * np.pi * X + phi) * R
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
+ax = fig.add_subplot(111, projection='3d')
 
-#Make the X, Y meshgrid.
+# Make the X, Y meshgrid.
 xs = np.linspace(-1, 1, 50)
 ys = np.linspace(-1, 1, 50)
 X, Y = np.meshgrid(xs, ys)
 
-#Set the z axis limits so they aren`t recalculated each frame.
+# Set the z axis limits so they aren't recalculated each frame.
 ax.set_zlim(-1, 1)
 
-
-#Begin plotting.
+# Begin plotting.
 wframe = None
 tstart = time.time()
 for phi in np.linspace(0, 180. / np.pi, 100):
-    #If a line collection is already remove it before drawing.
-
+    # If a line collection is already remove it before drawing.
     if wframe:
         ax.collections.remove(wframe)
 
-    #Plot the new wireframe and pause briefly before continuing
+    # Plot the new wireframe and pause briefly before continuing.
     Z = generate(X, Y, phi)
-    wframe = ax.plot_wireframe(X, Y, Z, rstride = 2, cstride = 2)
+    wframe = ax.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
     plt.pause(.001)
 
 print('Average FPS: %f' % (100 / (time.time() - tstart)))
