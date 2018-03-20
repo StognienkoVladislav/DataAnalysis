adf80d3df8d48f414da4afe0a7289416731aec29
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-20 14:28:44 +0200

    create directories

---
+++ Scraping/DataMining/WikiToCSV.py
@@ -0,0 +1,24 @@
+
+import csv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+html = urlopen("http://en.wikipedia.org/wiki/Comparison_of_text_editors")
+bsObj = BeautifulSoup(html)
+# В данный момент главная таблица сравнрений является первой таблицей на странице
+table = bsObj.findAll("table", {"class": "wikitable"})[0]
+rows = table.findAll("tr")
+
+
+csvFile = open("files/editors.csv", 'wt')
+writer = csv.writer(csvFile)
+
+try:
+    for row in rows:
+        csvRow = []
+        for cell in row.findAll(['td', 'th']):
+            csvRow.append(cell.get_text())
+        writer.writerow(csvRow)
+finally:
+    csvFile.close()
