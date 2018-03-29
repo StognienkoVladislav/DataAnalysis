de104a7616c766ceb1fefea1dfa18a9ee02d0112
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-29 15:05:47 +0300

    forms crawling

---
+++ Scraping/CrawlingWithWebForms/HTTP-logging.py
@@ -0,0 +1,9 @@
+
+import requests
+from requests.auth import AuthBase
+from requests.auth import HTTPBasicAuth
+
+
+auth = HTTPBasicAuth('test', 'password')
+r = requests.post(url="http://pythonscraping.com/pages/auth/login.php", auth=auth)
+print(r.text)
