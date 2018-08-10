9aa8f1df9dcba4c2a2f6821d69e97656e3c0c171
StognienkoVladislav <stognienkovv@gmail.com>
2018-08-10 23:51:20 +0300

    final

--- DA_Task/script.py
+++ DA_Task/script.py
@@ -1,12 +1,11 @@
-
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-
+from prophet import Prophet
 
 df = pd.read_csv('data.csv', sep=',')
 print(df.describe())
 
+# Общий анализ данных
 print('#################')
 print(df['registration_day'].describe())
 print('#################')
@@ -15,3 +14,40 @@ print('#################')
 print(df['source_id'].describe())
 print('#################')
 print(df['sum_net'].describe())
+
+df2 = df.copy()
+# Для 1 вида
+# df2 = df2[df2['source_id'] == 1]
+
+# Для 2 вида
+# df2 = df2[df2['source_id'] == 2]
+
+# Общий вариант
+
+df2 = df2.rename(columns={'payment_day': 'Days'})
+df2['Days'] = pd.DatetimeIndex(df2['Days'])
+
+# Визуализация текущих данных
+
+ax = df2.set_index('Days').plot(figsize=(12, 8))
+ax.set_ylabel("Payment value")
+ax.set_xlabel('Date')
+plt.show()
+
+# Прогнозирование
+my_model = Prophet(interval_width=0.95)
+my_model.fit(df2)
+
+future_dates = my_model.make_future_dataframe(periods=30, freq="day")
+print(future_dates.tail())
+
+forecast = my_model.predict(future_dates)
+
+# визуализации прогнозируемых данных
+# Prophet показывает значения временных рядов (черные точки),
+# прогнозируемые значения (синяя линия)
+# и интервалы неопределенности прогнозов (синие заштрихованные области).
+my_model.plot(forecast, uncertainty=True)
+
+# компоненты прогнозов
+my_model.plot_components(forecast)
