35c7e38b24a17a750fd3dd0d32c51ed0d341de01
StognienkoVladyslav <stognienkovv@gmail.com>
2018-09-11 16:31:29 +0300

    comments

--- Scraping/ScrapTask/script.py
+++ Scraping/ScrapTask/script.py
@@ -4,29 +4,34 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
+# Парсим из html страницы, ссылки на товары
 def get_hrefs(mass_url):
     hrefs = []
     for url in mass_url:
         html = urlopen(url)
-
+        # Считываем html
         bsObj = BeautifulSoup(html.read(), 'lxml')
         for sibling in bsObj.findAll("div", class_="featured-collection__info js-product-tile"):
+            # После нахождения контейнера, вытягиваем ссылки на товары
             hrefs.append(sibling.find('a').get('href'))
 
     return hrefs
 
 
+# Обрабатываем ссылки и относительно параметров (args) формируем JSON
 def get_info(hrefs, *args):
     info = {}
     columns = args[0]
     print(len(hrefs))
+    # В цикле проходимся по всем товарам
     for i, url in enumerate(hrefs):
         i += 1
+        # формируем url относительно базовой ссылки и уникальной части
         html = urlopen(base + url)
 
         bsObj = BeautifulSoup(html.read(), 'lxml')
         info[i] = {}
-
+        # находим div с нужной нам информацией и парсим ее относительно (args)
         for sibling in bsObj.findAll("div", class_="grid__item large-up--one-third product__selector-container"):
             sku = sibling.find("div", class_='product__sku').get_text()
             if 'SKU' in columns:
@@ -69,22 +74,23 @@ def get_info(hrefs, *args):
     return info
 
 
-pages = 2
+# Входные данные
+########################################################################################
 base = 'https://suzyshier.com'
 exclusives = 'https://suzyshier.com/collections/sz_trend_online-exclusives'
+all_params = ['SKU', 'title', 'price', 'color', 'size', 'description', 'specs']
 
 # Если больше 1 страницы
+pages = 2
 urls = []
 for page in range(pages):
     urls.append('https://suzyshier.com/collections/sz_bottoms_shop-all-bottoms?page={}'.format(page+1))
-
+########################################################################################
 # Для exclusives
 # hrefs = get_hrefs([exclusives])
 
 # Для bottoms
 hrefs = get_hrefs(urls)
-
-all_params = ['SKU', 'title', 'price', 'color', 'size', 'description', 'specs']
 
 # В массив добавляем нужные параметры или используем все (all_params)
 # full_info = get_info(hrefs, ['title', 'price', 'description', 'size'])
