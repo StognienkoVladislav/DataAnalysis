fe0982517be17b8bfee6c20ed03cb3dc7a2aad62
StognienkoVladislav <stognienkovv@gmail.com>
2019-05-10 18:08:36 +0300

    udemy intro ML course

---
+++ UdemyCourses/DeepLearningPrerequisites/sample_script.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+
+a = np.array([1, 2])
+b = np.array([2, 1])
+
+
+dot = 0
+
+print(np.sum(a*b))
+print((a*b).sum())
+print(np.dot(a, b))
+
+
+amag = np.sqrt((a*a).sum())
+print(amag)
+
+amag = np.linalg.norm(a)
+print(amag)
+
+
+cosangle = a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+print(cosangle)
+
+angle = np.arccos(cosangle)
+print(angle)
